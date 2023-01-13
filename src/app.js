b8f9dc5b8362a3b30e17d37bb1befddc608c4ecc
Crv86 <lil_criss69@hotmail.com>
2023-01-13 19:34:44 +0000

    RESUELTO2

--- src/app.js
+++ src/app.js
@@ -7,6 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  var pronoun = ['the','our', 'my'];
+  var adj = ['great', 'big', 'small' ];
+  var noun = ['jogger','racoon'];
 
-  console.log("Consola de Resultados");
+  let textHTML = '<ul class="list-group">';
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k=0; k < noun.length; k++) {
+        console.log(i, j, k)
+        const element1 = pronoun[i];
+        const element2 = adj[j];
+        const element3 = noun[k];
+let domainName = element1 + element2 + element3;
+        textHTML += '<li class="list-group-item">' + domainName + '</li>';
+      }
+    }
+  }
+
+  // 1. Generar todas las combinaciones de los tres arreglos
+  // n. Mostrar una lista con todas las combinaciones en html
+
+  textHTML += '</ul>';
+  
+  let element = document.querySelector('#domain')
+  element.innerHTML = textHTML
+
 };
